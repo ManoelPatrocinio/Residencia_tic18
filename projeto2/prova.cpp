e0d4e4de090a3bd4a0df7dcdfab983b044a4ec2c
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2023-08-28 16:40:44 -0300

    first commit

---
+++ projeto2/prova.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    float pi = 3.14;
+    int intPi = pi + 3;
+
+    float testFloat = 3.143;
+    int testInt = testFloat;
+
+    cout << "valor de intPi: " << intPi << endl;
+    cout << "valor de testInt: " << testInt << endl;
+    return 0;
+
+}
