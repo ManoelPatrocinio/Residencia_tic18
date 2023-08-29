d12d241db94b4b2b968614872ae6be7b0e084fdc
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2023-08-29 17:16:16 -0300

    add atv2908

---
+++ atv2908/questaoA.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+
+    char zero = '0',
+         um = '1',
+         dois = '2',
+         tres = '3',
+         quarto = '4',
+         cinco = '5',
+         seis = '6',
+         sete = '7',
+         oito = '8',
+         nov = '9';
+    int int_zero = zero,
+        int_um = um,
+        int_dois = dois,
+        int_tres = tres,
+        int_quarto = quarto,
+        int_cinco = cinco,
+        int_seis = seis,
+        int_sete = sete,
+        int_oito = oito,
+        int_nov = nov;
+
+    cout <<  zero << " - " << int_zero << endl ;   
+    cout <<  um << " - " << int_um << endl ;   
+    cout <<  dois << " - " << int_dois << endl ;   
+    cout <<  tres << " - " << int_tres << endl ;   
+    cout <<  quarto << " - " << int_quarto << endl ;   
+    cout <<  cinco << " - " << int_cinco << endl ;   
+    cout <<  seis << " - " << int_seis << endl ;   
+    cout <<  sete << " - " << int_sete << endl ;   
+    cout <<  oito << " - " << int_oito << endl ;   
+    cout <<  nov << " - " << int_nov << endl ;   
+
+    return 0;
+}
